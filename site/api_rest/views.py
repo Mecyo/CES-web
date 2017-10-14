c5be2f5dc4fc96cbe0880b09ddccf1a902ec7180
brunnomendes <brunnomendesinfo@gmail.com>
2017-10-14 13:10:28 -0300

    Copia do reposit[orio original para fins acadêmicos

---
+++ site/api_rest/views.py
@@ -0,0 +1,199 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from rest_framework.views import APIView
+from api_rest import serializers
+from ces import models
+from datetime import datetime
+
+
+class UsuariosServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        usuarios =  models.Usuario.objects.filter()
+        serializer = serializers.UsuarioSerializer(usuarios, many=True)
+        return Response(serializer.data)
+
+
+class UsuarioServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, usuario_id, format=None):
+        usuario =  models.Usuario.objects.get(id=usuario_id)
+        serializer = serializers.UsuarioSerializer(usuario)
+        return Response(serializer.data)
+
+
+class ObjetosServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        objetos =  models.Objeto.objects.filter()
+        serializer = serializers.ObjetoSerializer(objetos, many=True)
+        return Response(serializer.data)
+
+
+class ObjetoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, objeto_id, format=None):
+        objeto =  models.Objeto.objects.get(id=objeto_id)
+        serializer = serializers.ObjetoSerializer(objeto)
+        return Response(serializer.data)
+
+
+class ObjetoDisponivelServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, format=None):
+        objetos =  models.Objeto.objects.filter(status=1)
+        reservas = models.Objeto.objects.filter(data)
+        movimentacoes = models.Movimentacao.objects.filter(devolucao=None)
+        movimentacoes_objetos = []
+        objetos_list = []
+        for mov in movimentacoes:
+            movimentacoes_objetos.append(mov.objeto_id)
+        for obj in objetos:
+            if obj not in movimentacoes_objetos:
+                objetos_list.append(obj)
+        serializer = serializers.ObjetoSerializer(objetos_list, many=True)
+        return Response(serializer.data)
+
+
+class ObjetoDisponivelUsuarioServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, user_id, format=None):
+        objetos =  models.Objeto.objects.all()
+        movimentacoes = models.Movimentacao.objects.filter(devolucao=None)
+        movimentacoes_objetos = []
+        objetos_list = []
+        for mov in movimentacoes:
+            movimentacoes_objetos.append(mov.objeto_id)
+        for obj in objetos:
+            if obj not in movimentacoes_objetos:
+                # IF --> Verificar se o usuário tem permissão para exibir tal objeto
+                objetos_list.append(obj)
+        serializer = serializers.ObjetoSerializer(objetos_list, many=True)
+        return Response(serializer.data)
+
+
+class MovimentacaoUsuarioServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, user_id, format=None):
+        user = models.Usuario.objects.get(id=user_id)
+        movimentacoes =  models.Movimentacao.objects.filter(usuario_id__id=user.id)
+        serializer = serializers.MovimentacaoSerializer(movimentacoes, many=True)
+        return Response(serializer.data)
+
+
+class DetalheMovimentacaoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, movimentacao_id, format=None):
+        movimentacao =  models.Movimentacao.objects.get(id=movimentacao_id)
+        serializer = serializers.MovimentacaoSerializer(movimentacao)
+        return Response(serializer.data)
+
+
+class MovimentacaoAbertaUsuarioServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def get(self, request, user_id, format=None):
+        user = models.Usuario.objects.get(id=user_id)
+        movimentacoes =  models.Movimentacao.objects.filter(usuario_id__id=user.id, devolucao=None)
+        serializer = serializers.MovimentacaoSerializer(movimentacoes, many=True)
+        return Response(serializer.data)
+
+
+class EmprestarObjetoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        # PRECISA VERIFICAR SE O NOVO USUÁRIO TEM
+        # PERMISSÃO PARA PEGA O OBJETO ANTES DE TUDO
+        objeto_id = request.data.get('objeto_id')
+        objeto = models.Objeto.objects.get(id=objeto_id)
+        usuario_id = request.data.get('usuario_id')
+        usuario = models.Usuario.objects.get(id=usuario_id)
+
+        movimentacao = models.Movimentacao.objects.create(retirada=datetime.now(), devolucao=None, objeto_id=objeto,
+                                                          usuario_id=usuario, status=1)
+
+        serializer = serializers.MovimentacaoSerializer(movimentacao, many=True)
+        return Response(serializer.data)
+
+
+class DevolverObjetoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        movimentacao_id =  request.data.get('movimentacao_id')
+
+        movimentacao = models.Movimentacao.objects.get(id=movimentacao_id)
+        movimentacao.status = 3
+        movimentacao.devolucao = datetime.now()
+        movimentacao.save()
+
+        #serializer = serializers.MovimentacaoSerializer(movimentacao)
+        #return Response(serializer.data)
+        return Response(200)
+
+
+class TransferirObjetoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        # PRECISA VERIFICAR SE O NOVO USUÁRIO TEM
+        # PERMISSÃO PARA PEGAR O OBJETO ANTES DE TUDO
+        objeto_id = request.data.get('objeto_id')
+        objeto = models.Objeto.objects.get(id=objeto_id)
+        usuario_id = request.data.get('usuario_id')
+        usuario = models.Usuario.objects.get(id=usuario_id)
+        novo_usuario_id = request.data.get('novo_usuario_id')
+        novo_usuario = models.Usuario.objects.get(id=novo_usuario_id)
+
+        movimentacao_id =  request.data.get('movimentacao_id')
+        movimentacao = models.Movimentacao.objects.get(id=movimentacao_id)
+        movimentacao.devolucao = datetime.now()
+        movimentacao.save()
+
+        movimentacao = models.Movimentacao.objects.create(retirada = datetime.now(),devolucao=None,
+                                                              objeto_id=objeto, usuario_id=novo_usuario)
+        return Response(200)
+
+
+#class ConfirmarTransferenciaObjetoServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+ #   def post(self, request, format=None):
+
+
+class FiltroMovimentacaoUsuarioServiceView(APIView):
+    # permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        user = models.Usuario.objects.get(id=request.data.get('usuario'))
+        nome = request.data.get('nome_objeto')
+        retirada = request.data.get('data_retirada')
+        devolucao = request.data.get('data_devolucao')
+        status = request.data.get('status')
+        tipo = request.data.get('tipo_objeto')
+
+        movimentacoes =  models.Movimentacao.objects.filter(usuario_id__id=user.id).all()
+        if nome:
+            movimentacoes = movimentacoes.filter(objeto_id__nome__contains=nome).all()
+        if retirada:
+            retirada = datetime.strptime(retirada, '%d/%m/%Y')
+            movimentacoes = movimentacoes.filter(retirada__gte=retirada.date()).all()
+        if devolucao:
+            devolucao = datetime.strptime(devolucao, '%d/%m/%Y')
+            movimentacoes = movimentacoes.filter(devolucao__gte=devolucao.date()).all()
+        if status:
+            movimentacoes = movimentacoes.filter(status=status).all()
+        if tipo:
+            movimentacoes = movimentacoes.filter(objeto_id__tipoObjeto_id=tipo).all()
+
+        serializer = serializers.MovimentacaoSerializer(movimentacoes, many=True)
+        return Response(serializer.data)
---
+++ site/ces/models.py
@@ -0,0 +1,160 @@
+from django.db import models
+from django.utils import timezone
+from datetime import datetime
+from fontawesome.fields import IconField
+
+
+STATUS_MOVIMENTACAO = (
+    (1, 'Solicitado Retirada'),
+    (2, 'Emprestado'),
+    (3, 'Solicitado Devolução'),
+    (4, 'Devolvido'),
+    (5, 'Solicitado Transferência'),
+    (6, 'Transferência Pendente'),
+    (7, 'Transferência Confirmada'),
+    (8, 'Solicitado Reserva')
+)
+
+STATUS_OBJETO = (
+    (1, 'Disponível'),
+    (2, 'Indisponível'),
+    (3, 'Pendente'),
+)
+
+
+STATUS_MOVIMENTACOES = {
+    (1, 'Solicitado retirada'),
+    (2, 'Emprestado'),
+    (3, 'Solicitado Devolução'),
+    (4, 'Devolvido'),
+    (5, 'Solicitado Transferência'),
+    (6, 'Transferência confirmada'),
+    (7, 'Solicitado Reserva'),
+    (8, 'Reserva confirmada'),
+}
+
+
+class TipoObjeto(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    icone = IconField ()
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Tipo de objeto"
+        verbose_name_plural = "Tipos de objeto"
+
+
+class Objeto(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    tipoObjeto_id = models.ForeignKey(TipoObjeto)
+    status = models.IntegerField(choices=STATUS_OBJETO, default=1)
+
+    def __str__(self):
+        return self.nome
+
+
+class PerfilUsuario(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.nome
+
+    class Meta:
+        verbose_name = "Perfil de usuário"
+        verbose_name_plural = "Perfis de usuário"
+
+
+class Usuario(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    name = models.CharField(max_length=250, blank=True, null=True)
+    email = models.CharField(max_length=50, unique=True, blank=False,)
+    senha = models.TextField(blank=False, null=False)
+    perfilUsuario_id = models.ForeignKey(PerfilUsuario)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Usuário"
+        verbose_name_plural = "Usuários"
+
+
+class Movimentacao(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    retirada = models.DateTimeField(null=True, blank=True)
+    devolucao = models.DateTimeField(null=True, blank=True)
+    objeto_id = models.ForeignKey(Objeto)
+    usuario_id = models.ForeignKey(Usuario)
+    status = models.IntegerField(choices=STATUS_MOVIMENTACOES, default=0, blank=True)
+
+    status = models.IntegerField(choices=STATUS_MOVIMENTACAO, default=0)
+
+    def __str__(self):
+        return str("{0} para {1}").format(self.objeto_id, self.usuario_id)
+
+    class Meta:
+        verbose_name = "Movimentação"
+        verbose_name_plural = "Movimentações"
+
+
+class Permissao_Objeto_x_Usuario(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    objeto_id = models.ForeignKey(Objeto)
+    usuario_id = models.ForeignKey(Usuario)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Permissao_Objeto_x_PerfilUsuario(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    tipoObjeto_id = models.ForeignKey(TipoObjeto)
+    perfilUsuario_id = models.ForeignKey(PerfilUsuario)
+
+    def __str__(self):
+        return str(self.id)
+
+
+class Reserva(models.Model):
+    id = models.AutoField(primary_key=True, blank=False, null=False)
+    dataReserva = models.DateTimeField(null=True, blank=True)
+    objeto_id = models.ForeignKey(Objeto)
+    usuario_id = models.ForeignKey(Usuario)
+
+    def __str__(self):
+        return str("{0} reservou {1} para o dia {2}").format(self.usuario_id, self.objeto_id, self.dataReserva)
+
+    class Meta:
+        verbose_name = "Reserva"
+        verbose_name_plural = "Reservas"
+
+
+class Transferencia(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    remetente = models.ForeignKey(Movimentacao, related_name='remetente')
+    destinatario = models.ForeignKey(Movimentacao, related_name='destinatario')
+
+    def __str__(self):
+        return str("Transferindo {0} de {1} para {2}").format(self.remetente.objeto_id, self.remetente.usuario_id, self.destinatario.usuario_id)
+
+    class Meta:
+        verbose_name = "Transferência"
+        verbose_name_plural = "Transferências"
+
+
+class AdminWeb(models.Model):
+    id= models.AutoField(primary_key=True, blank=False, null=False)
+    tipoObjeto_id = models.ForeignKey(TipoObjeto)
+    usuario_id = models.ForeignKey(Usuario)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        verbose_name = "Admin web"
+        verbose_name_plural = "Admins web"
